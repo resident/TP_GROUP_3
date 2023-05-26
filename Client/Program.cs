ffc836178a0fa2d6910ec4420cce2071b300fe71
resident <resident@safe.vg>
2023-05-26 10:40:31 +0300

    Fixed invalid class name Client

--- Client/Program.cs
+++ Client/Program.cs
@@ -11,7 +11,7 @@ namespace Client
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Client());
+            Application.Run(new Form1());
         }
     }
 }
