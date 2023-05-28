403c52a1e25df382d993407302e973f7333519f3
resident <resident@safe.vg>
2023-05-28 19:39:06 +0300

    Add Alert classes to Server and Client projects

---
+++ Client/Alert.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal static class Alert
+    {
+        public static void Show(string message)
+        {
+            MessageBox.Show(message);
+        }
+
+        public static void Successful(string message)
+        {
+            MessageBox.Show(message, "Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        public static void Error(string message)
+        {
+            MessageBox.Show(message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
